a24f6470c5aa4ae86d7eba4ed0179195f94bbf1a
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2026-03-14 16:52:38 +0600

    Add files via upload

---
+++ js/var.js
@@ -0,0 +1,326 @@
+var lcsp1=new Array()
+lcsp1[0]=["http://www.lorrainechittock.com/lcimages/img/A1.jpg","",""]
+lcsp1[1]=["http://www.lorrainechittock.com/lcimages/img/A2.jpg","",""]
+lcsp1[2]=["http://www.lorrainechittock.com/lcimages/img/A3.jpg","",""]
+lcsp1[3]=["http://www.lorrainechittock.com/lcimages/img/A4.jpg","",""]
+lcsp1[4]=["http://www.lorrainechittock.com/lcimages/img/A5.jpg","",""]
+
+var lcsp2=new Array()
+lcsp2[0]=["http://www.lorrainechittock.com/lcimages/img/A6.jpg","",""]
+lcsp2[1]=["http://www.lorrainechittock.com/lcimages/img/A7.jpg","",""]
+lcsp2[2]=["http://www.lorrainechittock.com/lcimages/img/A8.jpg","",""]
+lcsp2[3]=["http://www.lorrainechittock.com/lcimages/img/A9.jpg","",""]
+lcsp2[4]=["http://www.lorrainechittock.com/lcimages/img/A10.jpg","",""]
+
+var lcsp3=new Array()
+lcsp3[0]=["http://www.lorrainechittock.com/lcimages/img/A11.jpg","",""]
+lcsp3[1]=["http://www.lorrainechittock.com/lcimages/img/A12.jpg","",""]
+lcsp3[2]=["http://www.lorrainechittock.com/lcimages/img/A13.jpg","",""]
+lcsp3[3]=["http://www.lorrainechittock.com/lcimages/img/A14.jpg","",""]
+lcsp3[4]=["http://www.lorrainechittock.com/lcimages/img/A15.jpg","",""]
+
+var lcin1=new Array()
+lcin1[0]=["http://www.lorrainechittock.com/lcimages/img/H1.jpg","",""]
+lcin1[1]=["http://www.lorrainechittock.com/lcimages/img/H2.jpg","",""]
+lcin1[2]=["http://www.lorrainechittock.com/lcimages/img/H3.jpg","",""]
+lcin1[3]=["http://www.lorrainechittock.com/lcimages/img/H4.jpg","",""]
+
+var lcin2=new Array()
+lcin2[0]=["http://www.lorrainechittock.com/lcimages/img/H5.jpg","",""]
+lcin2[1]=["http://www.lorrainechittock.com/lcimages/img/H6.jpg","",""]
+lcin2[2]=["http://www.lorrainechittock.com/lcimages/img/H7.jpg","",""]
+lcin2[3]=["http://www.lorrainechittock.com/lcimages/img/H8.jpg","",""]
+
+var lcin3=new Array()
+lcin3[0]=["http://www.lorrainechittock.com/lcimages/img/H9.jpg","",""]
+lcin3[1]=["http://www.lorrainechittock.com/lcimages/img/H10.jpg","",""]
+lcin3[2]=["http://www.lorrainechittock.com/lcimages/img/H11.jpg","",""]
+lcin3[3]=["http://www.lorrainechittock.com/lcimages/img/H12.jpg","",""]
+
+var lcbi1=new Array()
+lcbi1[0]=["http://www.lorrainechittock.com/lcimages/img/B1.jpg","",""]
+lcbi1[1]=["http://www.lorrainechittock.com/lcimages/img/B2.jpg","",""]
+lcbi1[2]=["http://www.lorrainechittock.com/lcimages/img/B3.jpg","",""]
+
+var lcbi2=new Array()
+lcbi2[0]=["http://www.lorrainechittock.com/lcimages/img/B4.jpg","",""]
+lcbi2[1]=["http://www.lorrainechittock.com/lcimages/img/B5.jpg","",""]
+lcbi2[2]=["http://www.lorrainechittock.com/lcimages/img/B6.jpg","",""]
+
+var lcbi3=new Array()
+lcbi3[0]=["http://www.lorrainechittock.com/lcimages/img/B7.jpg","",""]
+lcbi3[1]=["http://www.lorrainechittock.com/lcimages/img/B8.jpg","",""]
+lcbi3[2]=["http://www.lorrainechittock.com/lcimages/img/B9.jpg","",""]
+
+var lmsp1=new Array()
+lmsp1[0]=["http://www.lorrainechittock.com/lcimages/img/L1.jpg","",""]
+lmsp1[1]=["http://www.lorrainechittock.com/lcimages/img/L2.jpg","",""]
+lmsp1[2]=["http://www.lorrainechittock.com/lcimages/img/L3.jpg","",""]
+lmsp1[3]=["http://www.lorrainechittock.com/lcimages/img/L4.jpg","",""]
+lmsp1[4]=["http://www.lorrainechittock.com/lcimages/img/L5.jpg","",""]
+
+var lmsp2=new Array()
+lmsp2[0]=["http://www.lorrainechittock.com/lcimages/img/L6.jpg","",""]
+lmsp2[1]=["http://www.lorrainechittock.com/lcimages/img/L7.jpg","",""]
+lmsp2[2]=["http://www.lorrainechittock.com/lcimages/img/L8.jpg","",""]
+lmsp2[3]=["http://www.lorrainechittock.com/lcimages/img/L9.jpg","",""]
+lmsp2[4]=["http://www.lorrainechittock.com/lcimages/img/L10.jpg","",""]
+
+var lmsp3=new Array()
+lmsp3[0]=["http://www.lorrainechittock.com/lcimages/img/L11.jpg","",""]
+lmsp3[1]=["http://www.lorrainechittock.com/lcimages/img/L12.jpg","",""]
+lmsp3[2]=["http://www.lorrainechittock.com/lcimages/img/L13.jpg","",""]
+lmsp3[3]=["http://www.lorrainechittock.com/lcimages/img/L14.jpg","",""]
+lmsp3[4]=["http://www.lorrainechittock.com/lcimages/img/L15.jpg","",""]
+
+var ccsp1=new Array()
+ccsp1[0]=["http://www.lorrainechittock.com/lcimages/img/C1.jpg","",""]
+ccsp1[1]=["http://www.lorrainechittock.com/lcimages/img/C2.jpg","",""]
+ccsp1[2]=["http://www.lorrainechittock.com/lcimages/img/C3.jpg","",""]
+ccsp1[3]=["http://www.lorrainechittock.com/lcimages/img/C4.jpg","",""]
+ccsp1[4]=["http://www.lorrainechittock.com/lcimages/img/C5.jpg","",""]
+ccsp1[5]=["http://www.lorrainechittock.com/lcimages/img/C6.jpg","",""]
+
+var ccsp2=new Array()
+ccsp2[0]=["http://www.lorrainechittock.com/lcimages/img/C7.jpg","",""]
+ccsp2[1]=["http://www.lorrainechittock.com/lcimages/img/C8.jpg","",""]
+ccsp2[2]=["http://www.lorrainechittock.com/lcimages/img/C9.jpg","",""]
+ccsp2[3]=["http://www.lorrainechittock.com/lcimages/img/C10.jpg","",""]
+ccsp2[4]=["http://www.lorrainechittock.com/lcimages/img/C11.jpg","",""]
+ccsp2[5]=["http://www.lorrainechittock.com/lcimages/img/C12.jpg","",""]
+
+var ccsp3=new Array()
+ccsp3[0]=["http://www.lorrainechittock.com/lcimages/img/C13.jpg","",""]
+ccsp3[1]=["http://www.lorrainechittock.com/lcimages/img/C14.jpg","",""]
+ccsp3[2]=["http://www.lorrainechittock.com/lcimages/img/C15.jpg","",""]
+ccsp3[3]=["http://www.lorrainechittock.com/lcimages/img/C16.jpg","",""]
+ccsp3[4]=["http://www.lorrainechittock.com/lcimages/img/C17.jpg","",""]
+ccsp3[5]=["http://www.lorrainechittock.com/lcimages/img/C18.jpg","",""]
+
+var sssp1=new Array()
+sssp1[0]=["http://www.lorrainechittock.com/lcimages/img/S1.jpg","",""]
+sssp1[1]=["http://www.lorrainechittock.com/lcimages/img/S2.jpg","",""]
+sssp1[2]=["http://www.lorrainechittock.com/lcimages/img/S3.jpg","",""]
+sssp1[3]=["http://www.lorrainechittock.com/lcimages/img/S4.jpg","",""]
+sssp1[4]=["http://www.lorrainechittock.com/lcimages/img/S5.jpg","",""]
+
+var sssp2=new Array()
+sssp2[0]=["http://www.lorrainechittock.com/lcimages/img/S6.jpg","",""]
+sssp2[1]=["http://www.lorrainechittock.com/lcimages/img/S7.jpg","",""]
+sssp2[2]=["http://www.lorrainechittock.com/lcimages/img/S8.jpg","",""]
+sssp2[3]=["http://www.lorrainechittock.com/lcimages/img/S9.jpg","",""]
+sssp2[4]=["http://www.lorrainechittock.com/lcimages/img/S10.jpg","",""]
+
+var sssp3=new Array()
+sssp3[0]=["http://www.lorrainechittock.com/lcimages/img/S11.jpg","",""]
+sssp3[1]=["http://www.lorrainechittock.com/lcimages/img/S12.jpg","",""]
+sssp3[2]=["http://www.lorrainechittock.com/lcimages/img/S13.jpg","",""]
+sssp3[3]=["http://www.lorrainechittock.com/lcimages/img/S14.jpg","",""]
+sssp3[4]=["http://www.lorrainechittock.com/lcimages/img/S15.jpg","",""]
+var fadebgcolor="#FFF8DC"
+
+
+var omin1=new Array()
+omin1[0]=["http://www.lorrainechittock.com/lcimages/img/O1.jpg","",""]
+omin1[1]=["http://www.lorrainechittock.com/lcimages/img/O2.jpg","",""]
+omin1[2]=["http://www.lorrainechittock.com/lcimages/img/O3.jpg","",""]
+omin1[3]=["http://www.lorrainechittock.com/lcimages/img/O4.jpg","",""]
+omin1[4]=["http://www.lorrainechittock.com/lcimages/img/O5.jpg","",""]
+
+var omin2=new Array()
+omin2[0]=["http://www.lorrainechittock.com/lcimages/img/O6.jpg","",""]
+omin2[1]=["http://www.lorrainechittock.com/lcimages/img/O7.jpg","",""]
+omin2[2]=["http://www.lorrainechittock.com/lcimages/img/O8.jpg","",""]
+omin2[3]=["http://www.lorrainechittock.com/lcimages/img/O9.jpg","",""]
+omin2[4]=["http://www.lorrainechittock.com/lcimages/img/O10.jpg","",""]
+
+var omin3=new Array()
+omin3[0]=["http://www.lorrainechittock.com/lcimages/img/O11.jpg","",""]
+omin3[1]=["http://www.lorrainechittock.com/lcimages/img/O12.jpg","",""]
+omin3[2]=["http://www.lorrainechittock.com/lcimages/img/O13.jpg","",""]
+omin3[3]=["http://www.lorrainechittock.com/lcimages/img/O14.jpg","",""]
+omin3[4]=["http://www.lorrainechittock.com/lcimages/img/O15.jpg","",""]
+
+var fadebgcolor="#FFF8DC"
+
+
+
+var xxin1=new Array()
+xxin1[0]=["http://www.lorrainechittock.com/lcimages/img/X1.jpg","",""]
+xxin1[1]=["http://www.lorrainechittock.com/lcimages/img/X2.jpg","",""]
+xxin1[2]=["http://www.lorrainechittock.com/lcimages/img/X3.jpg","",""]
+xxin1[3]=["http://www.lorrainechittock.com/lcimages/img/X4.jpg","",""]
+xxin1[4]=["http://www.lorrainechittock.com/lcimages/img/X5.jpg","",""]
+
+var xxin2=new Array()
+xxin2[0]=["http://www.lorrainechittock.com/lcimages/img/X6.jpg","",""]
+xxin2[1]=["http://www.lorrainechittock.com/lcimages/img/X7.jpg","",""]
+xxin2[2]=["http://www.lorrainechittock.com/lcimages/img/X8.jpg","",""]
+xxin2[3]=["http://www.lorrainechittock.com/lcimages/img/X9.jpg","",""]
+xxin2[4]=["http://www.lorrainechittock.com/lcimages/img/X10.jpg","",""]
+
+var xxin3=new Array()
+xxin3[0]=["http://www.lorrainechittock.com/lcimages/img/X11.jpg","",""]
+xxin3[1]=["http://www.lorrainechittock.com/lcimages/img/X12.jpg","",""]
+xxin3[2]=["http://www.lorrainechittock.com/lcimages/img/X13.jpg","",""]
+xxin3[3]=["http://www.lorrainechittock.com/lcimages/img/X14.jpg","",""]
+xxin3[4]=["http://www.lorrainechittock.com/lcimages/img/X15.jpg","",""]
+
+var fadebgcolor="#FFF8DC"
+
+
+var rrin1=new Array()
+wlin1[0]=["http://www.lorrainechittock.com/lcimages/img/R1.jpg","",""]
+wlin1[1]=["http://www.lorrainechittock.com/lcimages/img/R2.jpg","",""]
+wlin1[2]=["http://www.lorrainechittock.com/lcimages/img/R3.jpg","",""]
+wlin1[3]=["http://www.lorrainechittock.com/lcimages/img/R4.jpg","",""]
+wlin1[4]=["http://www.lorrainechittock.com/lcimages/img/R5.jpg","",""]
+
+var rrin2=new Array()
+wlin2[0]=["http://www.lorrainechittock.com/lcimages/img/R6.jpg","",""]
+wlin2[1]=["http://www.lorrainechittock.com/lcimages/img/R7.jpg","",""]
+wlin2[2]=["http://www.lorrainechittock.com/lcimages/img/R8.jpg","",""]
+wlin2[3]=["http://www.lorrainechittock.com/lcimages/img/R9.jpg","",""]
+wlin2[4]=["http://www.lorrainechittock.com/lcimages/img/R10.jpg","",""]
+
+var rrin3=new Array()
+wlin3[0]=["http://www.lorrainechittock.com/lcimages/img/R11.jpg","",""]
+wlin3[1]=["http://www.lorrainechittock.com/lcimages/img/R12.jpg","",""]
+wlin3[2]=["http://www.lorrainechittock.com/lcimages/img/R13.jpg","",""]
+wlin3[3]=["http://www.lorrainechittock.com/lcimages/img/R14.jpg","",""]
+wlin3[4]=["http://www.lorrainechittock.com/lcimages/img/R15.jpg","",""]
+
+var fadebgcolor="#FFF8DC"
+
+
+
+////NO need to edit beyond here/////////////
+ 
+var fadearray=new Array() //array to cache fadeshow instances
+var fadeclear=new Array() //array to cache corresponding clearinterval pointers
+ 
+var dom=(document.getElementById) //modern dom browsers
+var iebrowser=document.all
+ 
+function fadeshow(theimages, fadewidth, fadeheight, borderwidth, delay, pause, displayorder){
+this.pausecheck=pause
+this.mouseovercheck=0
+this.delay=delay
+this.degree=10 //initial opacity degree (10%)
+this.curimageindex=0
+this.nextimageindex=1
+fadearray[fadearray.length]=this
+this.slideshowid=fadearray.length-1
+this.canvasbase="canvas"+this.slideshowid
+this.curcanvas=this.canvasbase+"_0"
+if (typeof displayorder!="undefined")
+theimages.sort(function() {return 0.5 - Math.random();}) //thanks to Mike (aka Mwinter) :)
+this.theimages=theimages
+this.imageborder=parseInt(borderwidth)
+this.postimages=new Array() //preload images
+for (p=0;p<theimages.length;p++){
+this.postimages[p]=new Image()
+this.postimages[p].src=theimages[p][0]
+}
+ 
+var fadewidth=fadewidth+this.imageborder*2
+var fadeheight=fadeheight+this.imageborder*2
+ 
+if (iebrowser&&dom||dom) //if IE5+ or modern browsers (ie: Firefox)
+document.write('<div id="master'+this.slideshowid+'" style="position:relative;width:'+fadewidth+'px;height:'+fadeheight+'px;overflow:hidden;"><div id="'+this.canvasbase+'_0" style="position:absolute;width:'+fadewidth+'px;height:'+fadeheight+'px;top:0;left:0;filter:progid:DXImageTransform.Microsoft.alpha(opacity=10);opacity:0.1;-moz-opacity:0.1;-khtml-opacity:0.1;background-color:'+fadebgcolor+'"></div><div id="'+this.canvasbase+'_1" style="position:absolute;width:'+fadewidth+'px;height:'+fadeheight+'px;top:0;left:0;filter:progid:DXImageTransform.Microsoft.alpha(opacity=10);opacity:0.1;-moz-opacity:0.1;-khtml-opacity:0.1;background-color:'+fadebgcolor+'"></div></div>')
+else
+document.write('<div><img name="defaultslide'+this.slideshowid+'" src="'+this.postimages[0].src+'"></div>')
+ 
+if (iebrowser&&dom||dom) //if IE5+ or modern browsers such as Firefox
+this.startit()
+else{
+this.curimageindex++
+setInterval("fadearray["+this.slideshowid+"].rotateimage()", this.delay)
+}
+}
+
+function fadepic(obj){
+if (obj.degree<100){
+obj.degree+=10
+if (obj.tempobj.filters&&obj.tempobj.filters[0]){
+if (typeof obj.tempobj.filters[0].opacity=="number") //if IE6+
+obj.tempobj.filters[0].opacity=obj.degree
+else //else if IE5.5-
+obj.tempobj.style.filter="alpha(opacity="+obj.degree+")"
+}
+else if (obj.tempobj.style.MozOpacity)
+obj.tempobj.style.MozOpacity=obj.degree/101
+else if (obj.tempobj.style.KhtmlOpacity)
+obj.tempobj.style.KhtmlOpacity=obj.degree/100
+else if (obj.tempobj.style.opacity&&!obj.tempobj.filters)
+obj.tempobj.style.opacity=obj.degree/101
+}
+else{
+clearInterval(fadeclear[obj.slideshowid])
+obj.nextcanvas=(obj.curcanvas==obj.canvasbase+"_0")? obj.canvasbase+"_0" : obj.canvasbase+"_1"
+obj.tempobj=iebrowser? iebrowser[obj.nextcanvas] : document.getElementById(obj.nextcanvas)
+obj.populateslide(obj.tempobj, obj.nextimageindex)
+obj.nextimageindex=(obj.nextimageindex<obj.postimages.length-1)? obj.nextimageindex+1 : 0
+setTimeout("fadearray["+obj.slideshowid+"].rotateimage()", obj.delay)
+}
+}
+ 
+fadeshow.prototype.populateslide=function(picobj, picindex){
+var slideHTML=""
+if (this.theimages[picindex][1]!="") //if associated link exists for image
+slideHTML='<a href="'+this.theimages[picindex][1]+'" target="'+this.theimages[picindex][2]+'">'
+slideHTML+='<img src="'+this.postimages[picindex].src+'" border="'+this.imageborder+'px">'
+if (this.theimages[picindex][1]!="") //if associated link exists for image
+slideHTML+='</a>'
+picobj.innerHTML=slideHTML
+}
+ 
+ 
+fadeshow.prototype.rotateimage=function(){
+if (this.pausecheck==1) //if pause onMouseover enabled, cache object
+var cacheobj=this
+if (this.mouseovercheck==1)
+setTimeout(function(){cacheobj.rotateimage()}, 100)
+else if (iebrowser&&dom||dom){
+this.resetit()
+var crossobj=this.tempobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+crossobj.style.zIndex++
+fadeclear[this.slideshowid]=setInterval("fadepic(fadearray["+this.slideshowid+"])",50)
+this.curcanvas=(this.curcanvas==this.canvasbase+"_0")? this.canvasbase+"_1" : this.canvasbase+"_0"
+}
+else{
+var ns4imgobj=document.images['defaultslide'+this.slideshowid]
+ns4imgobj.src=this.postimages[this.curimageindex].src
+}
+this.curimageindex=(this.curimageindex<this.postimages.length-1)? this.curimageindex+1 : 0
+}
+ 
+fadeshow.prototype.resetit=function(){
+this.degree=10
+var crossobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+if (crossobj.filters&&crossobj.filters[0]){
+if (typeof crossobj.filters[0].opacity=="number") //if IE6+
+crossobj.filters(0).opacity=this.degree
+else //else if IE5.5-
+crossobj.style.filter="alpha(opacity="+this.degree+")"
+}
+else if (crossobj.style.MozOpacity)
+crossobj.style.MozOpacity=this.degree/101
+else if (crossobj.style.KhtmlOpacity)
+crossobj.style.KhtmlOpacity=this.degree/100
+else if (crossobj.style.opacity&&!crossobj.filters)
+crossobj.style.opacity=this.degree/101
+}
+ 
+ 
+fadeshow.prototype.startit=function(){
+var crossobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+this.populateslide(crossobj, this.curimageindex)
+if (this.pausecheck==1){ //IF SLIDESHOW SHOULD PAUSE ONMOUSEOVER
+var cacheobj=this
+var crossobjcontainer=iebrowser? iebrowser["master"+this.slideshowid] : document.getElementById("master"+this.slideshowid)
+crossobjcontainer.onmouseover=function(){cacheobj.mouseovercheck=1}
+crossobjcontainer.onmouseout=function(){cacheobj.mouseovercheck=0}
+}
+this.rotateimage()
+}
